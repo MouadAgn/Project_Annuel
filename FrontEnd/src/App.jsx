abb6d3c8b8d19ea2d1a786374c33ea65c765ba9f
MouadAgn <aoughanemouad1@gmail.com>
2024-06-12 01:49:33 +0200

    SetUp App.jsx ( Routes )

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,12 +2,16 @@
 
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
+import AddFile from './pages/addFile'
+import ListFile from './pages/listFile'
 
 function App() {
   return (
     <>
       <Routes>
         {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/listFile" element={<ListFile />} />
+        <Route path="/addFile" element={<AddFile />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
