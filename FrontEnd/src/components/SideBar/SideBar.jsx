765725ee0beceb249d5843baacb33f28dccd4118
Andre <andre.coelho@hotmail.fr>
2024-08-27 10:01:09 +0200

    Delete border on button

--- FrontEnd/src/components/SideBar/SideBar.jsx
+++ FrontEnd/src/components/SideBar/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Home as HomeIcon, UserRoundPen, LogOut } from 'lucide-react';
+import { Home as HomeIcon, UserRoundPen, LogOut, FileCheck2 } from 'lucide-react';
 import './SideBar.css';
 
 const SideBar = () => {
@@ -9,6 +9,7 @@ const SideBar = () => {
         <ul>
           <li><a href="/home"><HomeIcon size={20} /><span>Home</span></a></li>
           <li><a href="/profile"><UserRoundPen size={20} /><span>Profile</span></a></li>
+          <li><a href="/invoices"><FileCheck2 size={20} /><span>Mes Factures</span></a></li>
           <li><a href="/logout"><LogOut size={20} /><span>Déconnexion</span></a></li>
         </ul>
       </nav>
